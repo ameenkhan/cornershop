e6c5d05ce79436a2cd0fb8746996a0ae807d939f
Ameen Khan <mak_24x7@hotmail.co.uk>
2020-05-22 11:50:20 -0400

    redux now used using react hooks

--- frontend/src/components/products/ProductItem.js
+++ frontend/src/components/products/ProductItem.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import {useSelector, useDispatch} from 'react-redux';
 
 const ProductItem = (props) => {
+  const counter = useSelector( (state) => state.cart )
+  const dispatch = useDispatch()
   return <>
     <h1>{ props.item.name }</h1>
     <h1>{ props.item.price }</h1>
@@ -10,6 +13,11 @@ const ProductItem = (props) => {
       title={ props.item.name }
       src={`/products/${props.item.image}`}
     />
+    <button
+      onClick={( () => dispatch({type: "ADD", payload: props.item}) )}
+    >
+      Add to cart
+    </button>
   </>
 }
 
